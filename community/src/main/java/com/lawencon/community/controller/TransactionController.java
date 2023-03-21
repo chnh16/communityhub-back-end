fb751dc79671a83de03d954d98fd9adacd5a2c13
malpisyahdan <alpisyahdan11@email.com>
2023-03-21 13:58:42 +0700

    Fix Transaction

--- community/src/main/java/com/lawencon/community/controller/TransactionController.java
+++ community/src/main/java/com/lawencon/community/controller/TransactionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -81,7 +82,7 @@ public class TransactionController {
 //		return new ResponseEntity<>(res, HttpStatus.OK);
 //	}
 	
-	@PutMapping
+	@PatchMapping
 	private ResponseEntity<PojoUpdateRes> updateRes(@RequestBody final PojoUpdateTransactionReq data){
 		final PojoUpdateRes res = transactionService.updateRes(data);
 		return new ResponseEntity<>(res, HttpStatus.OK);
